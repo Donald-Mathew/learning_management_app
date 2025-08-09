c2c5f625da1cc79df17a858a65c7ca74c7c9f98f
Donald <dontech909984@gmail.com>
2025-08-10 01:33:51 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import Cta from '@/components/CTA';
+import { recentSessions } from '@/constants';
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 
 
-const Page= () => {
-  return (
-      <div>
-    <h1 className="text-2-xl underline">Welcome to My Saas App</h1>
-      <Button>
-          Let's get started!
-      </Button>
-      </div>
-  )
+const Page= async() => {
+  
+  const companions  = await getAllCompanions({limit: 3});
+  const recentSessionsCompanions  = await getRecentSessions(10);
+   return (
+      <main>
+    <h1 className="text-2-xl underline">Popular Companions</h1>
+        
+         <section className="home-section">
+
+          {companions.map((companion) => (
+               <CompanionCard 
+               key={companion.id}
+                {...companion}
+                color={getSubjectColor(companion.subject)}
+              />
+          ))}
+              
+          
+         </section>
+
+         <section className='home-section'>
+           <CompanionsList 
+             title="Recently completed sessions"
+             companions={recentSessionsCompanions}
+             classNames="w-2/3 max-lg:w-full"
+           />
+           <Cta />
+          </section>
+      </main>
+   )
 }
 
 export default Page
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c81a54f77e91227efb7b6254f8b8ffb8@o4509672870182912.ingest.de.sentry.io/4509813366063184",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
